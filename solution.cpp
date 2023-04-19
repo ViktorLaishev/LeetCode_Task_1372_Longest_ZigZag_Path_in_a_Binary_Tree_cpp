b948f6981d007d6198c28cc929e5bc9bc5158a4f
ViktorLaishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-04-19 18:30:52 +0200

    Add files via upload

---
+++ solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int longestZigZag(TreeNode* root) {
+        int maxLen = 0;
+        dfs(root, true, 0, maxLen);
+        dfs(root, false, 0, maxLen);
+        return maxLen;
+    }
+    
+private:
+    void dfs(TreeNode* node, bool isLeft, int len, int& maxLen) {
+        if (!node) {
+            return;
+        }
+        
+        maxLen = max(maxLen, len);
+        
+        if (isLeft) {
+            dfs(node->left, false, len+1, maxLen);
+            dfs(node->right, true, 1, maxLen);
+        } else {
+            dfs(node->right, true, len+1, maxLen);
+            dfs(node->left, false, 1, maxLen);
+        }
+    }
+};
